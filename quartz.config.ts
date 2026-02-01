0af2ee3b63bdae3a343a60282d60562fd9874120
Alex Rionov <misterrio535@gmail.com>
2026-02-01 13:33:51 +0300

    Change pageTitle

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "DnD WIKI",
+    pageTitle: "IL - World Building",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
